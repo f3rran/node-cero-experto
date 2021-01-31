0ee5f7aefb8a6c1c9dc453be42c3d55fe0350ae5
f3rran <brechyt@gmail.com>
2021-01-31 11:24:35 +0100

    Clase 36

--- 4-Bases-node/app.js
+++ 4-Bases-node/app.js
@@ -1,23 +1,5 @@
 const { crearArchivo } = require('./multiplicar/multiplicar');
-const argv = require('yargs')
-    .option('b', {
-        alias: 'base',
-        type: 'number',
-        demandOption: true
-    })
-    .option('l', {
-        alias: 'listar',
-        type: 'boolean',
-        default: false
-    })
-    .check((argv, options) => {
-        
-        if (isNaN( argv.b)) {
-            throw 'La base tiene que ser un número';
-        }
-        return true;
-    })
-    .argv;
+const {argv} = require('./config/yargs');
 
 crearArchivo(argv.b, argv.l)
     .then(archivo =>console.log(`Archivo creado ${archivo}`))
---
+++ 4-Bases-node/config/yargs.js
@@ -0,0 +1,23 @@
+const argv = require('yargs')
+    .option('b', {
+        alias: 'base',
+        type: 'number',
+        demandOption: true,
+        describe: 'Es la base de la tabla de multiplicar'
+    })
+    .option('l', {
+        alias: 'listar',
+        type: 'boolean',
+        default: false,
+        describe: 'Muestra la tabla en consola'
+    })
+    .check((argv, options) => {
+        
+        if (isNaN( argv.b)) {
+            throw 'La base tiene que ser un número';
+        }
+        return true;
+    })
+    .argv;
+
+module.exports = {argv};
