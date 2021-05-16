c2f0b2d8317fc940f350718a2cbbe80e25f1d84a
f3rran <brechyt@gmail.com>
2021-05-16 19:45:05 +0200

    Completada sección 9: Rest Server con CRUD

---
+++ 9-restserver_alcance/models/server.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const cors = require('cors');
+const { dbConnection } = require('../database/config');
+
+class Server {
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+        this.usuariosPath = '/api/usuarios';
+
+        //Connectar a la base de datos
+        this.connectarDB();
+
+        //Middlewares
+        this.middlewares();
+
+        this.routes();
+    }
+
+    async connectarDB() {
+        await dbConnection();
+    }
+
+    middlewares(){
+
+        //Cors
+        this.app.use(cors());
+
+        //lectura y parseo del body
+        this.app.use(express.json());
+
+        //Directorio público
+        this.app.use( express.static('public'));
+    }
+
+    routes() {
+        this.app.use(this.usuariosPath, require('../routes/user'));
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log('Servidor corriendo en puerto: ', this.port);
+        });
+    }
+}
+
+module.exports = Server;
